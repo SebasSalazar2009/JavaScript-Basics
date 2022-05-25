1d2d60760b67be86ccec06faa80432ec00a5b9b7
Sebastian Salazar <sebastiansalazar@Sebastians-MacBook-Air.local>
2022-05-24 21:10:14 -0400

    basics

---
+++ index.js
@@ -0,0 +1,17 @@
+let name = 'sebas'; // string literal
+let age = 30; // number literal
+let isApproved = false; // boolean literal
+let firstName = undefined // 
+let selectedColor = null; //
+
+
+
+
+
+
+
+
+
+
+
+
